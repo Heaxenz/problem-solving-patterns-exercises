9a643afa6bbd1931ad90b8e7fa1826ba9a1c7f5c
Juan Robledo <heaxenz@Rikos-Air.lan>
2023-10-03 16:50:31 -0700

    update

--- src/dsa-sorting/insertion.js
+++ src/dsa-sorting/insertion.js
@@ -1,3 +1,15 @@
-function insertionSort() {}
+const insertionSort = (arr) => {
+    for(let i = 1; i < arr.length; i++){
+        let temp = arr[i]
+        let j = i - 1;
+        console.log(temp, arr[j])
+        while(j >= 0 && arr[j] > temp){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = temp;
+    }
+    return arr
+}
 
 module.exports = insertionSort;
